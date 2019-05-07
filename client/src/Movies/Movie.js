0b48e91ec6dcc421e4b79be76a6d0679a8f79372
Kayla <6lackmacaroon@gmail.com>
2019-05-07 12:08:21 -0600

    half a stretch, got movie card build and rendering  movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-// import MovieCard from './MovieCard'
+import MovieCard from './MovieCard'
 
 export default class Movie extends Component {
   constructor(props) {
@@ -26,7 +26,7 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
+/////// Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
   //   if(this.props.match.params.id !== newProps.match.params.id){
   //     this.fetchMovie(newProps.match.params.id);
@@ -37,7 +37,7 @@ export default class Movie extends Component {
   //   const addToSavedList = this.props.addToSavedList;
   //   addToSavedList(this.state.movie)
   // }
-
+/////////////////////////////////////
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -46,23 +46,8 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
-        </div>
-        {/* <MovieCard movie={movie} */}
+        
+        <MovieCard movies={this.state.movie}/>
         <div className="save-button">Save</div>
       </div>
     );
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const MovieCard = props => {
+  console.log('movie card props', props);
   return (
   <div className="movie-card">
-    <h2>{title}</h2>
+    <h2>{props.movies.title}</h2>
     <div className="movie-director">
-      Director: <em>{director}</em>
+      Director: <em>{props.movies.director}</em>
     </div>
     <div className="movie-metascore">
-      Metascore: <strong>{metascore}</strong>
+      Metascore: <strong>{props. movies.metascore}</strong>
     </div>
     <h3>Actors</h3>
 
-    {stars.map(star => (
+    {props.movies.stars.map(star => (
       <div key={star} className="movie-star">
         {star}
       </div>
